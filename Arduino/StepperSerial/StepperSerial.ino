50cb674c38baf1b81a9ccbbfcf694609f2d23c2d
Olivier Devauchelle <olivier@leechong.ipgp.fr>
2022-12-07 16:48:57 +0100

    First commit

---
+++ Arduino/StepperSerial/StepperSerial.ino
@@ -0,0 +1,138 @@
+/*
+O. Devauchelle, P. Delorme, A. Abramian, E. Lajeunesse & F. Metivier
+GNU General Public License v3.0, 2022
+*/
+
+
+const int dirA = 13;
+const int dirB = 12;
+
+const int motA = 11;
+const int motB = 3;
+
+const int brakeA = 8;
+const int brakeB = 9;
+
+float delayLength = -1; // negative value = stop
+int nStep = -1; // negative = infinite number of steps
+int RotDir = +1; // rotation direction 
+String UserCommand = "?";
+int CurrentStep = 0;
+
+char* string2char(String command){
+    if(command.length()!=0){
+        char *p = const_cast<char*>(command.c_str());
+        return p;
+    }
+}
+
+void PrintParams(){    
+      Serial.print("dt:");
+      Serial.print(delayLength);
+      Serial.print(", N:");
+      Serial.print(nStep);
+      Serial.print(", direction:");
+      Serial.print(RotDir);
+      Serial.print(", currentN:");
+      Serial.println(CurrentStep);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(dirA, OUTPUT);
+  pinMode(dirB, OUTPUT);
+  pinMode(brakeA, OUTPUT);
+  pinMode(brakeB, OUTPUT);
+  pinMode(motA, OUTPUT);
+  pinMode(motB, OUTPUT);
+
+}
+
+void MotorStep(int iStep){
+  
+  if ( iStep == 0 ){
+    digitalWrite(brakeA, HIGH);
+    digitalWrite(brakeB, LOW);
+    digitalWrite(dirB, LOW);
+    digitalWrite(motB, HIGH);
+  }
+  
+  else if ( iStep == 1 ){   
+    digitalWrite(brakeA, LOW);
+    digitalWrite(brakeB, HIGH);
+    digitalWrite(dirA, LOW);
+    digitalWrite(motA, HIGH);
+  }
+  
+  else if ( iStep == 2 ){
+    digitalWrite(brakeA, HIGH);
+    digitalWrite(brakeB, LOW);
+    digitalWrite(dirB, HIGH);
+    digitalWrite(motB, HIGH);
+  }
+  
+  else if ( iStep == 3 ){
+    digitalWrite(brakeA, LOW);
+    digitalWrite(brakeB, HIGH);
+    digitalWrite(dirA, HIGH);
+    digitalWrite(motA, HIGH);
+  }
+}
+
+
+void loop() {
+
+  if (Serial.available()> 0){
+    
+    UserCommand = Serial.readString();
+     
+    if ( UserCommand == "?" ){
+       PrintParams();
+    }
+
+    else if ( UserCommand == "s" ){
+       Serial.println( "Stopping." );
+       delayLength = -1;
+    }
+    
+    else {
+      
+      char * splitCommand = strtok ( string2char( UserCommand ), ">," );
+      
+      delayLength = atof( splitCommand );
+    
+      splitCommand = strtok ( NULL, "," );
+      nStep = atoi( splitCommand );
+
+      splitCommand = strtok ( NULL, "," );
+      RotDir = atoi( splitCommand );
+      
+      CurrentStep = 0;
+      
+      PrintParams();
+      
+    }
+    
+  }
+
+  if (delayLength > 0 & CurrentStep < nStep ){
+    
+    if ( RotDir >= 0 ){
+      for ( int iStep=0; iStep<=3; iStep++ ){
+        MotorStep(iStep);
+        delay(delayLength);
+      }
+    }
+ 
+    else {
+      for ( int iStep=3; iStep>=0; iStep-- ){
+        MotorStep(iStep);
+        delay(delayLength);
+      }
+    }
+
+    CurrentStep += 1;
+    
+  };
+
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# StepperMotorSerial
+
+Control a stepper motor with an Arduino via Serial.
+
+## Serial commands
+
+- `?` Get current state.
+
+- `s` Stop the motor.
+
+- `>>200,10,-1` Run the motor backwards for ten steps with a 200 ms time interval. 
