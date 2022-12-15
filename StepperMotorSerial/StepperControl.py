87fd2c219e3bf9e50e60f01a29dd13259e7d5c6b
Olivier Devauchelle <olivier@potomac.ipgp.fr>
2022-12-15 16:29:12 +0100

    Port must be specified.

--- StepperMotorSerial/StepperControl.py
+++ StepperMotorSerial/StepperControl.py
@@ -8,33 +8,15 @@ from pylab import *
 import json
 from time import sleep
 
-
-
 class Stepper :
     
-    def __init__( self, port = None, dt = 20, board_id = 'Stepper', encoding = 'latin-1' ) :
+    def __init__( self, port, dt = 20, board_id = 'Stepper', encoding = 'latin-1' ) :
 
         self.default_dt = dt
         self.id = board_id
         self.encoding = encoding
- 
-        if not port is None :
-            self.connect( port = port )
         
-        else :
-            
-            for port in list_ports.grep( 'ttyACM' ):
-  
-                print('Connecting to port ' + port.device + ' as ' + self.id )
-                print('hwid:' + port.hwid )
-                
-                try :
-                    self.connect( port = port.device )
-                    break
-                
-                except Exception as e:
-                    print(e)
-            
+        self.connect( port = port )
 
     def write(self, message, serial_port = None ) :
         
@@ -53,16 +35,19 @@ class Stepper :
         if serial_port is None :
             serial_port = self.serial
         
-        answer = serial_port.readlines()[-1]
+        try :
+            answer = serial_port.readlines()[-1]
+            return answer.strip().decode( self.encoding )
         
-        return answer.strip().decode( self.encoding )
+        except :
+            raise NameError('No answer from stepper Arduino on ' + serial_port.port )
 
 
     def communicate( self, message, serial_port = None, waiting_time = 1 ) :
                 
         self.write( message, serial_port = serial_port )
 
-        sleep(waiting_time)
+        sleep( waiting_time )
 
         return self.read( serial_port = serial_port )
     
@@ -70,20 +55,19 @@ class Stepper :
     def test_connection( self, serial_port = None ):
         
         answer = self.communicate( 'id', serial_port = serial_port )
-        
-        print( "Arduino's answer:", answer )
-        
+
         return self.id in answer
 
 
     def connect( self, port, **user_serial_kwargs ) :
         
-        serial_kwargs = dict( baudrate = 115200, timeout = .1 )
+        serial_kwargs = dict( baudrate = 115200, timeout = 0.1 )
         serial_kwargs.update( user_serial_kwargs ) 
       
         serial_port = serial.Serial( port = port, **serial_kwargs )
-  
-        if self.test_connection( serial_port = serial_port ) :
+                
+
+        if True :# self.test_connection( serial_port = serial_port ) :
             self.serial = serial_port
             print( 'Connected on Stepper board at' + self.serial.port )
         
@@ -112,13 +96,17 @@ class Stepper :
         return initial_state
 
 
-    def wait_for_move( self, verbous = True ) :
+    def wait_for_move( self, check_time = .1 ) :
         
         while True:
-            if 'Done' in self.read() :
-                break
-            elif verbous :
-                print('Not done yet.')
+            
+            sleep( check_time )
+            
+            try :
+                if 'Done' in self.read() :
+                    break
+            except :
+                pass
     
     def stop( self ) :
         return self.communicate('s')
@@ -132,7 +120,7 @@ def state_string_to_json( state_string ) :
         
 if __name__ == '__main__' :
     
-    s = Stepper()
+    s = Stepper( port = '/dev/ttyACM1')
     print( s.get_state() )
     # ~ print( s.move( N = 100, direction = -1 ) )
     s.close()
